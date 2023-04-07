18ef5bfe6149b523844bc7b2dff16fd0f792a7d0
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 06:51:29 +0900

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
+cd ..
+git clone https://github.com/wildan-pratama/wildan-hyprland.git
