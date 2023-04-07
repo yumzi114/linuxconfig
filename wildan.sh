a4098ca5cd217611b9792c1c71ca6660bab131ba
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 06:17:37 +0900

    Create wildan.sh

---
+++ wildan.sh
@@ -0,0 +1,8 @@
+
+#!/bin/sh
+yay -Syy sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
+swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
+bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
+inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
+qt5-quickcontrols2 brightnessctl noto-fonts-main pipewire wireplumber grim slurp jq dunst \
+qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
