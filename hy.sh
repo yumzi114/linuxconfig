faaa57436d3fc7feffbdd6f41b95bc62f6cbb22a
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 06:54:00 +0900

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -5,3 +5,4 @@ makepkg -si
 yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
 cd ..
 git clone https://github.com/wildan-pratama/wildan-hyprland.git
+chmod +x wildan.sh
