51b569d6e8d5a2f4db807b9ad00a173f679f76d9
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 09:15:01 +0900

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -5,6 +5,10 @@ makepkg -si
 yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
 sudo pacman -S xf86-video-intel fzf
 cd ..
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+sudo make install
+cd..
 git clone https://github.com/wildan-pratama/wildan-hyprland.git
 chmod +x wildan.sh
 ./wildan.sh
