4e17b029d0abfdf79e296f31695f05965a9c9105
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-09 06:30:18 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -8,7 +8,7 @@ cd ..
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
-yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info chromium
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info chromium base-devel
 sudo pacman -S xf86-video-intel fzf
 yay -S neofech xdg-desktop-portal-hyprland 
 cd ..
