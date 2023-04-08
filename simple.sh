8ea5608c94865224b123d32f506fcc921903232d
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-09 06:37:24 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -8,22 +8,22 @@ cd ..
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
-yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info chromium base-devel
+yay -Sy gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info chromium base-devel
 sudo pacman -S xf86-video-intel fzf
-yay -S neofech xdg-desktop-portal-hyprland 
+yay -Sy neofech xdg-desktop-portal-hyprland 
 cd ..
 git clone https://github.com/wildan-pratama/wildan-hyprland.git
 cd wildan-hyprland
 
 ./install.sh
-yay -S sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
-swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
-bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
-inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
-qt5-quickcontrols2 brightnessctl pipewire wireplumber grim slurp jq dunst \
-qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
+#yay -Sy sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
+#swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
+#bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
+#inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
+#qt5-quickcontrols2 brightnessctl pipewire wireplumber grim slurp jq dunst \
+#qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
 
-yay -S thunar thunar-archive-pluginfile-roller mpv geany viewnior mpc mpd ncmpcpp chromium neofetch sway
+yay -Sy thunar thunar-archive-pluginfile-roller mpv geany viewnior mpc mpd ncmpcpp chromium neofetch sway
 sudo cp -r usr /
 sudo cp -r etc /
 cp -r .config ~/
