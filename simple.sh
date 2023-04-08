1004dc96932e10352b9b8730a887e2ea0391d6c7
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 13:08:34 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -15,6 +15,15 @@ git clone https://github.com/wildan-pratama/wildan-hyprland.git
 cd wildan-hyprland
 
 ./install.sh
+yay -Syy sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
+swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
+bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
+inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
+qt5-quickcontrols2 brightnessctl pipewire wireplumber grim slurp jq dunst \
+qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
+
+yay -Syy thunar thunar-archive-pluginfile-roller mpv geany viewnior mpc mpd ncmpcpp
 cd ..
 sudo cp hyprland.conf ~/.config/hypr/
 sudo cp windows-rule.conf ~/.config/hypr/
+
