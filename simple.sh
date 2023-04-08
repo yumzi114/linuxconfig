ecc4cd64e45cf8221d9fe3660321a04a300c1363
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-08 17:55:10 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -16,12 +16,12 @@ git clone https://github.com/wildan-pratama/wildan-hyprland.git
 cd wildan-hyprland
 
 ./install.sh
-#yay -S sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
-#swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
-#bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
-#inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
-#qt5-quickcontrols2 brightnessctl pipewire wireplumber grim slurp jq dunst \
-#qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
+yay -S sddm-git polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git \
+swayidle  swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder \
+bc gtk-engine-murrine xfce-polkit dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg \
+inetutils pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects hyprland-scratchpad-git \
+qt5-quickcontrols2 brightnessctl pipewire wireplumber grim slurp jq dunst \
+qt5-wayland qt6-wayland pastel networkmanager network-manager-applet noto-fonts-emoji wdisplays swww
 
 yay -S thunar thunar-archive-pluginfile-roller mpv geany viewnior mpc mpd ncmpcpp
 sudo systemctl enable sddm.service
