ba8e6e1745c5d6853aa9b500bb1ff9cd4db58abd
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-10 03:04:15 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-mkdir build && cd build
-git clone https://aur.archlinux.org/linux-vfio.git
-cd linux-vfio
-makepkg -si
-cd ..
 git clone https://github.com/wildan-pratama/wildan-hyprland.git
 cd wildan-hyprland
 ./install.sh
@@ -15,6 +10,8 @@ cp -r .local ~/
 cp -r .icons ~/
 cp -r .gtkrc-2.0 ~/
 cd ..
+sudo cp hyprland.conf ~/.config/hypr/
+sudo cp windows-rule.conf ~/.config/hypr/
 #yay -Sy gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info 
 sudo pacman -S xf86-video-intel
 yay -Sy neofech xdg-desktop-portal-hyprland chromium fzf
