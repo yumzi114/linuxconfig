95d71550c6fae0cee95e060ce201b49f714a2d0f
yum <95202277+yumzi114@users.noreply.github.com>
2023-04-09 09:21:25 +0900

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -10,7 +10,7 @@ cd yay-bin
 makepkg -si
 #yay -Sy gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info 
 sudo pacman -S xf86-video-intel fzf
-yay -Sy neofech xdg-desktop-portal-hyprland chromium base-devel
+yay -Sy neofech xdg-desktop-portal-hyprland chromium base-devel hyprland-git
 cd ..
 git clone https://github.com/wildan-pratama/wildan-hyprland.git
 cd wildan-hyprland
