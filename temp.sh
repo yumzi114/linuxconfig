eed56417063e211d3e897578a382eb65b4c2d537
yum <95202277+yumzi114@users.noreply.github.com>
2023-05-15 00:38:25 +0900

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,6 @@
+paru -S rustup go chezmoi hyprland-git xdg-desktop-portal-hyprland-git grim polkit-gnome ffmpeg swaybg ttf-font-awesome \
+rofi-git pavucontrol qt5ct zsh wev wl-clipboard wf-recorder kwallet-pam cliphist jaq ripgrep btop moreutils \
+swaybg grimblast-git ffmpegthumbnailer playerctl dictd qtkeychain-qt6 flameshot-git batify2-git \
+noise-suppression-for-voice lf spotify-player libdisplay-info spotifywm-git \
+eww-wayland wlogout swaylock-effects-git sddm-git pamixer neofetch espanso-wayland-git discord betterdiscordctl-git dplymouth\
+papirus-icon-theme-git swayosd-git dunst wezterm swayidle geticons udiskie python-pywal cpupower-git sddm-theme-astronaut
