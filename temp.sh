b67b6b4b0232562bb236755388845b645ffcf5f3
yum <95202277+yumzi114@users.noreply.github.com>
2023-05-15 01:44:34 +0900

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+yay -S pokemon-colorscripts-git hyprland-git hyprpicker-git xdg-desktop-portal-hyprland-git waybar-hyprland cava kitty wofi starship wl-clipboard swaync swaylock-effects-git swaylockd tty-clock-git playerctl pavucontrol btop mpd mpd-mpris mpv mpv-mpris qt5-base qt5-wayland qt6-base qt6-wayland lsd geany bat cliphist-bin gamemode polkit-gnome g4music wlogout visual-studio-code-bin boo-grub-git sddm-git boo-sddm-git proxzima-plymouth-git yad blueman network-manager-applet libinput-gestures light --needed
+
+sudo pacman -R xdg-desktop-portal-wlr
+yay pacman -S xdg-desktop-portal-hyprland-git
