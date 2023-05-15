7ef4c4be87a79e119d1d9ecec8e99135ee93c3c5
yum <95202277+yumzi114@users.noreply.github.com>
2023-05-15 12:57:37 +0900

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -2,7 +2,7 @@
 yay -S pokemon-colorscripts-git hyprland-git hyprpicker-git xdg-desktop-portal-hyprland-git waybar-hyprland cava kitty wofi starship wl-clipboard swaync swaylock-effects-git swaylockd tty-clock-git playerctl pavucontrol btop mpd mpd-mpris mpv mpv-mpris qt5-base qt5-wayland qt6-base qt6-wayland lsd geany bat cliphist-bin gamemode polkit-gnome g4music wlogout visual-studio-code-bin boo-grub-git sddm-git boo-sddm-git proxzima-plymouth-git yad blueman network-manager-applet libinput-gestures light --needed
 
 sudo pacman -R xdg-desktop-portal-wlr
-yay pacman -S xdg-desktop-portal-hyprland-git
+yay -S xdg-desktop-portal-hyprland-git
 yay -S system76-power obs-studio wlrobs-hg v4l2loopback-dkms v4l2loopback-utils v4l-utils waydroid binder_linux-dkms chromium-wayland-vaapi qalculate-gtk qimgv-light rlr-git uget libreoffice-fresh nemo bulky --needed
 sudo pacman -S gtk-engine-murrine
 cd ~/Downloads
@@ -20,8 +20,8 @@ sudo cp -r kde/cursors/Sweet-cursors /usr/share/icons/
 cd ~/Downloads
 git clone https://github.com/PROxZIMA/.dotfiles.git
 cd .dotfiles
-cp -r .config .icons .local .mpd .ncmpcpp .scripts ~/
-cp .face .fonts.conf .dmrc .gtkrc-2.0 ~/
+sudo cp -r .config .icons .local .mpd .ncmpcpp .scripts ~/
+sudo cp .face .fonts.conf .dmrc .gtkrc-2.0 ~/
 chmod -R +x ~/.scripts/
 chmod +x ~/.config/hypr/autostart
 chmod -R +x ~/.config/wlogout/
